9f8bbf447833bb310a54871c70102ec8e73ac989
Hannah Chauvet <hannahchauvet@Hannahs-MacBook-Pro.local>
2017-10-05 21:25:35 -0700

    Submit Homework 05

--- hw05/scripts/part1.js
+++ hw05/scripts/part1.js
@@ -3,22 +3,6 @@ let languageCode = 'en'
 let jsonData
 
 
-const setLanguage = (code) => {
-    //Note: language codes here: https://www.w3schools.com/tags/ref_language_codes.asp
-    languageCode = code
-    if (code === 'ru') {
-        language = 'Russian'
-    } else if (code === 'es') {
-        language = 'Spanish'
-    } else {
-        language = 'English'
-    }
-    document.getElementById('language').innerHTML = language
-
-    //clear form:
-    getData()
-}
-
 const clearData = () => {
     const element = document.getElementById('results')
     while (element.firstChild) {
@@ -26,7 +10,67 @@ const clearData = () => {
     }
 }
 
-//Note: language codes here: https://www.w3schools.com/tags/ref_language_codes.asp
+const setLanguage = (code) => {
+    //Note: language codes here: https://www.w3schools.com/tags/ref_language_codes.asp
+    languageCode = code
+    if (code === 'ru') {language = 'Russian'}
+    else if (code === 'es') {language = 'Spanish'}
+    else {language = 'English'}
+    document.getElementById('language').innerHTML = language
+
+    //clear form:
+    getData()
+}
+
+// ---------------------- TASK 1 -----------------------------------
+// 1. Write a function called 'reverseText' that takes a single
+// string as an input. It should return that string with all
+// characters reversed e.g., 'Hello, world' will return 'dlrow ,olleH'
+// You can test this in the browser by updating the forEach function
+// to reverse every tweet's text.
+// HINT: This is a common interview question to screen out javascript
+// devs who don't understand the language's native functions.
+// Google around - you might find a good solution for this problem.
+// Once you've written your reverse text function, write a comment
+//describing how it works.
+
+//This first attempt works now that I know where to use it
+const reverseText1 = (text) => {
+  let newStr = ''
+  for (let i = text.length-1; i >= 0;i--)
+  {
+    newStr = newStr + text[i]
+  }
+  return newStr;
+}
+
+//Second attempt also works fine when used in the proper place
+const reverseText2 = (str) => {
+    let txt = ''
+    txt = str
+    return txt.split('').reverse().join('')
+}
+
+
+
+// ---------------------- TASK 2 -----------------------------------
+// 2. Now, write a function called 'reverseTweet' that takes a single
+// string as an input. If the string contains a hashtag (#), it should
+//return the string. If it doesn't contain a hashtag, it should return
+// the string with all characters reversed.
+// Update the forEach function so that it reverses tweets that don't
+// have hashtags
+// HINT: Strings have a useful function called indexOf that you may
+// find useful: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
+// HINT: You already have a reverseText function laying around...
+
+// ---------------------- DELIVERABLE -----------------------------------
+// When somebody searches for a keyword, tweets with hashtags will
+//appear as normal. Tweets with no hashtags will be inexplicably reversed.
+// EXTRA CREDIT: Add a class to reversed tweets to make their background
+// red. This will require modifying the existing CSS!
+
+
 const getData = () => {
 
     const term = document.getElementById('term').value
@@ -38,7 +82,8 @@ const getData = () => {
         })
         .then(function(json) {
             let div
-            let textNode
+            let textNode = ''
+            let temp = ''
 
             // output statuses:
             console.log(json.statuses)
@@ -46,28 +91,14 @@ const getData = () => {
             // clear out existing tweets:
             clearData()
 
-            // ---------------------- TASK 1 -----------------------------------
-            // 1. Write a function called 'reverseText' that takes a single string as an input. It should return that string with all characters reversed
-            // e.g., 'Hello, world' will return 'dlrow ,olleH'
-            // You can test this in the browser by updating the forEach function to reverse every tweet's text
-            // HINT: This is a common interview question to screen out javascript devs who don't understand the language's native functions. Google around - you might find a good solution for this problem
-            // Once you've written your reverse text function, write a comment describing how it works.
-
-            // ---------------------- TASK 2 -----------------------------------
-            // 2. Now, write a function called 'reverseTweet' that takes a single string as an input. If the string contains a hashtag (#), it should return the string.
-            // If it doesn't contain a hashtag, it should return the string with all characters reversed
-            // Update the forEach function so that it reverses tweets that don't have hashtags
-            // HINT: Strings have a useful function called indexOf that you may find useful: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
-            // HINT: You already have a reverseText function laying around...
-
-            // ---------------------- DELIVERABLE -----------------------------------
-            // When somebody searches for a keyword, tweets with hashtags will appear as normal. Tweets with no hashtags will be inexplicably reversed.
-            // EXTRA CREDIT: Add a class to reversed tweets to make their background red. This will require modifying the existing CSS!
-
             json.statuses.forEach((status) => {
                 div = document.createElement('div')
                 div.className = 'tweet'
-                textNode = document.createTextNode(status.text)
+
+            // When Sarah showed me where to call function then both
+            // the attempts worked
+                textNode = document.createTextNode(reverseText2(status.text))
+
                 div.appendChild(textNode)
                 document.getElementById('results').appendChild(div)
             })
