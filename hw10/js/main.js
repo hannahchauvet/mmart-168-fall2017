3b4a6be4542606f69229765894982ad2cd438404
Hannah Chauvet <hannahchauvet@yahoo.com>
2017-12-07 17:21:52 -0800

    completed homework 10

--- hw10/js/main.js
+++ hw10/js/main.js
@@ -3,31 +3,29 @@ const apiKey = 'MW9S-E7SL-26DU-VV8V'
 const makeStationList = () => {
     const url = 'https://api.bart.gov/api/stn.aspx?key=' + apiKey +
                     '&cmd=stns&json=y'
-    fetch(url)
-        .then((response) => {
+    const stationList = fetch(url)
+          .then((response) => {
             return response.json()
-        })
-        .then((json) => {
-            json = json.root
-            console.log(json)
-
-            // PART III.A.: Use a loop to populate the select menu with *ALL*
-            // of the stations that are returned from the BART data feed:
-            const option1 = document.createElement("option")
-            option1.value = 'DBRK'
-            option1.innerHTML = 'Downtown Berkeley'
-            document.getElementById('station_list').appendChild(option1)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+            })
+          .then((json) => {
+                json.root.stations.station.forEach((station) => {
+                const option = document.createElement("option")
+                option.value = station.abbr
+                option.innerHTML = station.name
+                document.getElementById('station_list').appendChild(option)
+              })
+            })
+            .catch((err) => {
+                console.log(err)
+            })
 }
 
 const getArrivalTimes = () => {
     const stationList = document.getElementById('station_list')
     // PART III.B.1: The bartStationCode should read from the list and query
     // for the corresponding station
-    const bartStationCode = 'DBRK'
+
+    const bartStationCode = stationList.value
     console.log('Selected Station Code:', bartStationCode)
     let url = 'https://api.bart.gov/api/etd.aspx?key=' + apiKey + '&cmd=etd' +
                 '&orig=' + bartStationCode + '&json=y'
@@ -41,35 +39,44 @@ const getArrivalTimes = () => {
             const results = document.getElementById('results')
             results.innerHTML = ''
             json.station = json.station[0]
-            if (!Array.isArray(json.station.etd)) {
-                json.station.etd = [ json.station.etd ]
-            }
-            json.station.etd.forEach(trainLine => {
-                if (!Array.isArray(trainLine.estimate)) {
-                    trainLine.estimate = [ trainLine.estimate ]
-                }
-                // PART III.B.2: Instead of printing this info to the console,
-                // output it to the DOM
-                console.log('------------------------------------------------------------------------')
-                console.log('FROM:', stationList.options[stationList.selectedIndex].text.toUpperCase())
-                console.log('TO:', trainLine.destination.toUpperCase())
-                console.log('------------------------------------------------------------------------')
-                trainLine.estimate.forEach(estimate => {
-                    // PART III.B.2. Instead of printing this info to the console,
-                    // output it to the DOM
-                    console.log(
-                        ' * Direction:', estimate.direction,
-                        ', Leaving: ', estimate.minutes,
-                        ', Color: ', estimate.hexcolor,
-                        ', Platform:', estimate.platform,
-                        ', Delay?:', estimate.delay
-                    )
-                })
+            let selectedStation = document.createElement('h2')
+            let stationName = document.createTextNode(json.station.name)
+            selectedStation.appendChild(stationName)
+            let delayWarning = document.createElement('p')
+            let delayCodeText = document.createTextNode('*** indicates possible delay')
+            delayWarning.appendChild(delayCodeText)
+            results.appendChild(selectedStation)
+            results.appendChild(delayWarning)
+            json.station.etd.forEach((trainLine) => {
+                let originDest = document.createElement('h3')
+                let originDestText = document.createTextNode('From ' +
+                stationList.options[stationList.selectedIndex].text.toUpperCase() +
+                '  to ' + trainLine.destination.toUpperCase())
+                originDest.appendChild(originDestText)
+                results.appendChild(originDest)
+                trainLine.estimate.forEach((estimate) => {
+                  let est = document.createElement('p')
+                  let eText = document.createTextNode('Leaving from Platform ' + estimate.platform)
+                  est.appendChild(eText)
+                  eText = document.createTextNode(' due ' + estimate.direction + ': ')
+                  est.appendChild(eText)
+                  if (estimate.minutes === 'Leaving')
+                  {
+                    eText = document.createTextNode('now ')
+                  }
+                  else {
+                    eText = document.createTextNode(' ' + estimate.minutes + ' mins. ')
+                  }
+                  est.appendChild(eText)
+                  if (estimate.delay !== '0')
+                  {
+                    eText = document.createTextNode('***' + ' ')
+                  }
+                  est.appendChild(eText)
+                  est.style.background = estimate.hexcolor
+                  results.appendChild(est)
+                  })
+              })
             })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
 }
-
 makeStationList()
